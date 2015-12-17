c15413c16dde1a7c15e861a50762d42b745b3c7a
weprikjm <ruben.idigora@gmail.com>
2015-12-17 22:57:43 +0100

    Build plane and stuff

--- Physics3D_class6 - solution/ModuleSceneIntro.h
+++ Physics3D_class6 - solution/ModuleSceneIntro.h
@@ -23,9 +23,15 @@ public:
 
 	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
 	//Platform Factory
-	Cube* createPlatform(vec3 position, vec3 size,PhysBody3D* pBody = NULL, bool isSensor = false, int check = -1, bool isVisible = true, float rot = 0.0f, float rot_x = 1.0f,
+	Cube* createPlatform(vec3 position, vec3 size, PhysBody3D*& pBody, bool isSensor = false, int check = -1, bool isVisible = true, float rot = 0.0f, float rot_x = 1.0f,
 						float rot_y = 0.0f, float rot_z = 0.0f, float mass = 0.0f);
-	bool AttachPlatform(PhysBody3D& obj, PhysBody3D& obj2, Cube& cObj1, Cube& cObj2, Orientation orientation);
+	bool AttachPlatform(PhysBody3D* obj, PhysBody3D* obj2, Cube* cObj1, Cube* cObj2, Orientation orientation);
+
+	void CreatePlane(vec3 positionPlane, vec3 sizeTile, int rows, int cols, float angle = 0.0f, vec3 rotation = {0.0f,0.0f,0.0f});
+
+	void CreateCurve45(vec3 pos, vec3 sizeTile, int rows, float angle);
+
+	void CreateCurve30(vec3 pos, vec3 sizeTile, int rows, float angle);
 
 public:
 	
